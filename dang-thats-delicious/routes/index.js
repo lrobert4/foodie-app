6e9cbbfce420759aac98be19dff88f05ad6ebf91
leer <ladesigns90@gmail.com>
2020-05-04 23:50:51 -0400

    initial commit for all project files.

---
+++ dang-thats-delicious/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+  const wes = { name: 'Wes, age: 100, cool: true' }
+  // res.send('Hey! It works!');
+  // res.json(wes);
+  res.send(req.query.name);
+  
+});
+
+router.get('/reverse/:name', (req,res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+});
+
+module.exports = router;
