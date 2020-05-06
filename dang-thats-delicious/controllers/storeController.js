9a4a5ec558322d3d802c9abe41cb450047da19b7
leer <ladesigns90@gmail.com>
2020-05-06 18:19:15 -0400

    Added the Asynce await to createStore controller, but still form handling functionality not working like tutorial. Will need to do further debugging.

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,13 +1,19 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index'); 
 };
 
 exports.addStore = (req, res) => {
+    console.log(req.name);
     res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    console.log(req.name);
+    const store = new Store(req.body);
+    await store.save();
+    res.redirect('/');
 };
